3898f93acad3ee52553506b4cb0ea3d955c68c31
Ren Wang <iamwrm@gmail.com>
2022-11-07 06:24:35 +0000

    ssh works

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-docker build -t dev_container .
+DOCKER_BUILDKIT=1 docker build -t dev_container .
+
+docker save dev_container | sudo k3s ctr images import -
+
+sudo k3s ctr i ls
+
---
+++ run_k8s.sh
@@ -0,0 +1,5 @@
+sudo kubectl apply -f k8s/dev_container.yml
+
+## Setup passwd 
+## sudo kubectl exec -it pod/dev-5c45448fb4-r729w bash
+
