d3bbe626e02ebf9d59a9862b62a97b3670905a2d
Ren Wang <iamwrm@gmail.com>
2022-11-07 08:29:03 +0000

    kasm works

--- 2_run_k8s.sh
+++ 2_run_k8s.sh
@@ -4,7 +4,13 @@ cp user_home/* /home/ubuntu/dev_home/
 
 
 sudo kubectl apply -f k8s/dev_container.yml
+sudo kubectl apply -f k8s/kasm.yml
 
 ## Setup passwd 
-## sudo kubectl exec -it deploy/dev -- bash -c "passwd ubuntu"
+sudo kubectl exec -it deploy/dev -- bash -c "passwd ubuntu"
+
+
+## apply policy once the container is up
+sudo kubectl apply -f k8s/kasm.yml
+
 
