d3b995b1606a4063245e075c1969e42a3fc7f670
Ren Wang <iamwrm@gmail.com>
2022-11-07 07:56:29 +0000

    works

---
+++ 0_install_k3s.sh
@@ -0,0 +1,17 @@
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--flannel-backend=none --disable-network-policy' sh -
+
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
+CLI_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+
+
+cilium install
+
+cilium status --wait
+
+
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # container_dev
 
+## DONE
+- dev
+- kasm
+
+## TODO
+- cilium network policy
 
 
