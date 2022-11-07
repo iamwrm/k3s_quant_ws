0fbcf9118f51c9f6713370be2742862de49c1be0
Ren Wang <iamwrm@gmail.com>
2022-11-07 06:36:03 +0000

    user_home fine

--- run_k8s.sh
+++ run_k8s.sh
@@ -1,5 +1,10 @@
+## prepare dev_home
+mkdir -p /home/ubuntu/dev_home
+cp user_home/* /home/ubuntu/dev_home/
+
+
 sudo kubectl apply -f k8s/dev_container.yml
 
 ## Setup passwd 
-## sudo kubectl exec -it pod/dev-5c45448fb4-r729w bash
+## sudo kubectl exec -it deploy/dev -- bash -c "passwd ubuntu"
 
